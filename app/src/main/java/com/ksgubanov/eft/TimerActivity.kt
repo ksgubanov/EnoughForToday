060d14552c06e4e5e04517752682c4da150a30cf
KGubanov <ksgubanov@gmail.com>
2020-08-30 12:06:16 +0200

    Add TimerActivity

---
+++ app/src/main/java/com/ksgubanov/eft/TimerActivity.kt
@@ -0,0 +1,15 @@
+package com.ksgubanov.eft
+
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+
+class TimerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
